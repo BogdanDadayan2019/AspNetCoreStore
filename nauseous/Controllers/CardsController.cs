d762c9e8d2aba438c42c3228a437b5cb03614851
DESKTOP-HHISQE3\bogda <bogdandadayan@gmail.com>
2021-04-06 15:05:19 +0300

    upd

---
+++ nauseous/Controllers/CardsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using nauseous.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+
+namespace nauseous.Controllers
+{
+    public class CardsController : Controller
+    {
+        [Route("cards/{id}")]
+        public IActionResult card1(int id)
+        {
+
+            switch (id)
+            {
+                case 1:
+                    
+                    ViewBag.Clothes = new Clothes { Id = 1, Name = "X-RAY", Price = 750, ImgPath = "/images/hood.jpg" };
+                    break;
+
+                case 2:
+                    
+                    ViewBag.Clothes = new Clothes { Id = 2, Name = "Ali", Price = 500, ImgPath = "/images/tshirt.jpg" };
+                    break;
+
+                case 3:
+                    ViewBag.Clothes = new Clothes { Id = 3, Name = "Life is Nauseous", Price = 500, ImgPath = "/images/cap.jpg" }; 
+                    break;
+
+
+                default:
+                    break;
+
+            }
+        
+
+            return View();
+        }
+
+
+
+    }
+}
---
+++ nauseous/Models/Clothes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nauseous.Models
+{
+    public class Clothes
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+                
+        public int Price { get; set; }
+
+        public string ImgPath { get; set; }
+       
+    }
+}
